eabf0bdbf995558ea90a80b2b05345db050e6ace
Baltej Singh <baltej963@gmail.com>
2025-03-01 15:07:49 +0530

    UI Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20" style={{fontFamily:"monospace"}}>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Image src='vercel.svg' width={10} height={10} alt="Vercel Logo" /> */}
-        <h1 className="text-4xl">Cooking Something</h1>
-        <p className="justify-items-center"><a style={{opacity:0.5}} href="https://github.com/baltej223">@baltej223</a></p>
-        </main>
-        {/* <Image src="/logo.svg" alt="Logo" width={100} height={100} /> */}
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row"> */}
-          {/* </div> */}
+    <div className="h-screen w-full flex flex-row justify-center items-center">
+      <main className="flex flex-col gap-5 md:gap-8 justify-center" style={{fontFamily:"monospace"}}>
+        <div className="text-4xl w-full flex justify-center">
+          <h1 className="text-white">Cooking Something</h1>
+          </div>
+        <p className="pr-5 flex w-full justify-center md:justify-end"><a style={{opacity:0.5}} href="https://github.com/baltej223">@baltej223</a></p>
+      </main>
     </div>
   );
 }
