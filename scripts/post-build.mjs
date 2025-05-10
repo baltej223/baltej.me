1feb21b194ac29b451ff64fea49dfefb6b9a9b7f
baltej223 <baltej963@gmail.com>
2025-05-10 10:50:11 +0530

    Fixed Some Errors

--- scripts/post-build.mjs
+++ scripts/post-build.mjs
@@ -6,20 +6,7 @@ fs.readFile('./CHANGELOG.md', 'utf8', (err, data) => {
     }
     // const updatedData = data.replace(/## \[Unreleased\]/, '## [Unreleased]\n\n## [New Release] - ' + new Date().toISOString().split('T')[0]);
 
-    let html = 
-    `
-    <html>
-    <head>
-    <title>Change Log</title>
-    </head>
-    <body>
-    ${data}
-    <script src='https://baltej223.github.io/md_interpreter/main.js'></script>
-    </body>
-    </html>
-    `;
-
-    fs.writeFile('./public/changelog.md', html, 'utf8', (writeErr) => {
+    fs.writeFile('./public/changelog.md', data, 'utf8', (writeErr) => {
         if (writeErr) {
             throw new Error('Error writing file: ' + writeErr);
         }
